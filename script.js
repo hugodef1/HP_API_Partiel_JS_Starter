1773a5fa6a37f35f1df41bc0c57601046d5b7196
hugo defaye <hugodef33@gmail.com>
2025-10-16 10:41:09 +0200

    Partie 2.1 Ajout intégration dynamique des personnages

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const response = await fetch(`https://hp-api.onrender.com/api/characters`);
+const data = await response.json();
+
+const charactersDiv = document.querySelector(".characters");
+charactersDiv.innerHTML = data.slice(0,12).map(character => `
+  <div class="charac" data-charac="${character.name}" house="${character.house}">
+    <a href="details.html?char=${encodeURIComponent(character.name)}">
+      <img class="${character.house.toLowerCase()}-img" src="${character.image}" alt="${character.name}" />
+    </a>
+    <p>${character.name}</p>
+  </div>
+`).join("");
