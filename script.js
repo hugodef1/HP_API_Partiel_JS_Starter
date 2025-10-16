d5847207a20a7c16b6090424ed51344c8d350a91
hugo defaye <hugodef33@gmail.com>
2025-10-16 11:12:14 +0200

    Partie 2.2 Trie par maison et dans l'ordre alphabétique

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const response = await fetch(`https://hp-api.onrender.com/api/characters`);
 const data = await response.json();
-
 const charactersDiv = document.querySelector(".characters");
-charactersDiv.innerHTML = data.slice(0,12).map(character => `
-  <div class="charac" data-charac="${character.name}" house="${character.house}">
-    <a href="details.html?char=${encodeURIComponent(character.name)}">
-      <img class="${character.house.toLowerCase()}-img" src="${character.image}" alt="${character.name}" />
-    </a>
-    <p>${character.name}</p>
-  </div>
-`).join("");
+
+function displayCharacters(characters) {
+  charactersDiv.innerHTML = characters.map(character => `
+    <div class="charac" data-charac="${character.name}" house="${character.house}">
+      <a href="details.html?char=${encodeURIComponent(character.name)}">
+        <img class="${character.house.toLowerCase() + '-img'}" src="${character.image}" alt="${character.name}" />
+      </a>
+      <p>${character.name}</p>
+    </div>
+  `).join("");
+}
+
+function filterCharacters(house, sorted ) {
+    const filtered = house === "All" ? sorted : sorted.filter(character => character.house === house);
+    displayCharacters(filtered.slice(0, 12));
+}
+
+displayCharacters(data.slice(0, 12));
+
+const filterButtons = document.querySelectorAll(".filter-button");
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const house = button.getAttribute("data-house");
+    const sorted = [...data.slice(0, 12)].sort((a, b) => a.name.localeCompare(b.name));
+    filterCharacters(house, sorted);
+  });
+});
